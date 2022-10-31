7af2d561e399c334a9ca055e05ea4bc7b82db851
amolmane7471 <maneamol3233@gmail.com>
2022-10-31 22:06:07 +0530

    [Amol] : added UC1 my first servlet

--- src/main/java/com/bridgelabz/FirstServlet.java
+++ src/main/java/com/bridgelabz/FirstServlet.java
@@ -1,19 +1,19 @@
-//package com.bridgelabz;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//@WebServlet("/Hello")
-//public class FirstServlet extends HttpServlet {
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
-//        response.setContentType("text/html");
-//        PrintWriter out = response.getWriter();
-//        out.println("<h3>Hello amol my first servlet page </h3>");
-//        out.close();
-//    }
-//}
+package com.bridgelabz;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Hello")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello amol this is my first servlet page </h3>");
+        out.close();
+    }
+}
